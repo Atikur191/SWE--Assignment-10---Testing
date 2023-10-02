a8e9220b7d630876576dc5b2f84c7d10821072a7
Atikur191 <127894097+Atikur191@users.noreply.github.com>
2023-10-01 20:45:06 -0500

    Add files via upload

---
+++ src/main/java/edu/miu/cs425/Testing/ArrayFlattenerTestSuite.java
@@ -0,0 +1,10 @@
+package edu.miu.cs425.Testing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ ArrayFlattenerTest.class })
+public class ArrayFlattenerTestSuite {
+
+}
---
+++ src/main/java/edu/miu/cs425/TestingPart2/ArrayFlattenerService.java
@@ -0,0 +1,6 @@
+package edu.miu.cs425.TestingPart2;
+
+public interface ArrayFlattenerService
+{
+    int[] flattenArray(int[][] nestedArray);
+}
